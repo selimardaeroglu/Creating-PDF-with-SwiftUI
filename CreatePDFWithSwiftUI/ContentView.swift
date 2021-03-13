bc8ab406fa340715d5890b02fdf2d93217dc8e5f
Selim Arda Eroğlu <selimardaeroglu@gmail.com>
2021-03-13 08:59:44 +0300

    Initial Commit

---
+++ CreatePDFWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CreatePDFWithSwiftUI
+//
+//  Created by Selim Arda Eroğlu on 13.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreatePDFWithSwiftUI/CreatePDFWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatePDFWithSwiftUIApp.swift
+//  CreatePDFWithSwiftUI
+//
+//  Created by Selim Arda Eroğlu on 13.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct CreatePDFWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
